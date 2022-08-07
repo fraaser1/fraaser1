3b6d2fe99df16e15f6c730739e62c67493620837
aldebaran2022 <110170715+aldebaran2022@users.noreply.github.com>
2022-08-07 17:04:07 +0200

    Update mein_konto.php

--- mein_konto.php
+++ mein_konto.php
@@ -9,15 +9,13 @@ $spaltennamen = array("ID","Vorname","Nachname","Benutzername","Email","Anmeldez
 $sql = "select * from benutzer where id = ? and benutzername = ?;";
 $stmt = mysqli_prepare($link, $sql);
 mysqli_stmt_bind_param($stmt, 'is', $id, $benutzername);
-/*function test_input($data) {
-	//entfernt alle ASCII außer Buchstaben,also auch Zahlen
-	$data = trim($data,"\x00..\x2F\x3A..\x40\x7B..\x7F\x5B..\x60");
-	return $data;
-	*/
+
 function test_input($data) {
 	  $data = trim($data);	//entferne space, tab und newline
 	  $data = stripslashes($data);	//entferne backslash (macht nur den Backslash vor einem Backslash weg weg
 	  $data = htmlspecialchars($data); //maskiere < und >
+	  //entfernt alle ASCII außer Buchstaben,also auch Zahlen
+	  //$data = trim($data,"\x00..\x2F\x3A..\x40\x7B..\x7F\x5B..\x60");
 	  return $data;
 }
 
@@ -58,30 +56,28 @@ function erstelle_Tabelle($spaltennamen,$ausgabe){
 }
 //hier wäre alternativ auch isset möglich:
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-	if (empty($_POST["benutzername"])) {
+	 if (empty($_POST["benutzername"])) {
 		$benutzernameERR = "Eingabe eines Benutzernamens ist erforderlich!";
 	  } else {
 			$benutzername = test_input($_POST["benutzername"]);					
-				if (!preg_match("/^[ÄÜÖäüößa-zA-Z-]*[0-9]*$/",$benutzername)) {
-				  $benutzernameERR = "Es sind nur Buchstaben und Zahlen erlaubt";
-				}else{
-					$zaehler +=1;
-					//speichere, dass Eingabe ok
-				}
-			
+			if (!preg_match("/^[ÄÜÖäüößa-zA-Z-]*[0-9]*$/",$benutzername)) {
+			  $benutzernameERR = "Es sind nur Buchstaben und Zahlen erlaubt";
+			}else{
+				$zaehler +=1;
+				//speichere, dass Eingabe ok
+			}			
 	  }
 	if (empty($_POST["id"])) {
 		$idErr = "Eingabe einer ID ist erforderlich!";
 	  } else {  
-			$id = test_input($_POST["id"]);							
-				if (!preg_match("/^[0-9]*$/",$id)) {
-					  $idErr = "Es sind nur Zahlen erlaubt";
-				}else{
-					$zaehler +=1;
-					//speichere, dass Eingabe ok
-				}
+		$id = test_input($_POST["id"]);							
+			if (!preg_match("/^[0-9]*$/",$id)) {
+				  $idErr = "Es sind nur Zahlen erlaubt";
+			}else{
+				$zaehler +=1;
+				//speichere, dass Eingabe ok
+			}
 	  }
-
 }
 
 ?>
@@ -89,11 +85,11 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 <!DOCTYPE html>
 <html lang="de">
 <head> 
-		 <meta charset="utf-8">
-		 <meta id="viewport" name="viewport" content="width=device-width, initial-scale=1.0">
-			<link rel="icon" type="image/icon" sizes="16x16" href="favicon.ico">
-			<link rel="stylesheet" href="quiz.css"> 
-			<link rel="stylesheet" href="kontakte.css">
+	 <meta charset="utf-8">
+	 <meta id="viewport" name="viewport" content="width=device-width, initial-scale=1.0">
+		<link rel="icon" type="image/icon" sizes="16x16" href="favicon.ico">
+		<link rel="stylesheet" href="quiz.css"> 
+		<link rel="stylesheet" href="kontakte.css">
 			
 <style>
 	table {
@@ -156,22 +152,21 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 		<h2 class="text-dark"> Teste dein Wissen?</h2>	
 	</div>
 </header>
-<div style="color:white;font-size:1.1em;">
-	
-		<form method=POST action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
-			<label for="benutzername" style="color:white;">Bitte geben Sie Ihren Benutzernamen ein:
-			<b class="error"  style="color:red;">* <?php echo "<br>".$benutzernameERR;?></b></label>
-			<input name="benutzername" type="text" class="feedback-input" placeholder="Benutzername"  value="<?php echo $benutzername;?>">
-			
-			
-			<label for="id" style="color:white;">Bitte geben Sie Ihre ID ein: <b class="error"  style="color:red;">* <?php echo "<br>".$idErr;?></b></label>		
-			<input name="id" type="number" class="feedback-input" placeholder="ID"  min="1" max="1000" value="<?php echo $id;?>">
-				
-			<input type="submit" value="Absenden">
-			<br>
-			<b class="error"  style="color:red;">* Pflichtfeld</b>
-		</form>
-	<div style="text-align:center;color:white;font-size:1.1em;">	
+<div style="color:white;font-size:1.1em;">	
+	<form method=POST action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+		<label for="benutzername" style="color:white;">Bitte geben Sie Ihren Benutzernamen ein:
+		<b class="error"  style="color:red;">* <?php echo "<br>".$benutzernameERR;?></b></label>
+		<input name="benutzername" type="text" class="feedback-input" placeholder="Benutzername"  value="<?php echo $benutzername;?>">
+
+
+		<label for="id" style="color:white;">Bitte geben Sie Ihre ID ein: <b class="error"  style="color:red;">* <?php echo "<br>".$idErr;?></b></label>		
+		<input name="id" type="number" class="feedback-input" placeholder="ID"  min="1" max="1000" value="<?php echo $id;?>">
+
+		<input type="submit" value="Absenden">
+		<br>
+		<b class="error"  style="color:red;">* Pflichtfeld</b>
+	</form>
+<div style="text-align:center;color:white;font-size:1.1em;">	
 		<?php
 			if ($zaehler == 2){
 				if(mysqli_stmt_execute($stmt)){
