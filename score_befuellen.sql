43b2e56b9a44de93bb37a0f0208c64a3235705f9
aldebaran2022 <110170715+aldebaran2022@users.noreply.github.com>
2022-08-08 09:35:52 +0200

    Add files via upload
    Hier sind alle Datenbank-Skripte und die Bilder für unsere Test-Benutzer

---
+++ score_befuellen.sql
@@ -0,0 +1,7 @@
+insert into score values
+(0,"Da ist aber noch nachholbedarf"),
+(1,"Oh Oh war wohl nicht dein Tag"),
+(2,"Da ist aber noch Luft nach oben"),
+(3,"Weiter so, aber könnte noch besser sein"),
+(4,"Langsam wird's"),
+(5,"Na also, geht doch");
