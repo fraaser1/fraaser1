e77208d76f3f553be2db39f4ce18b768c01b86c9
aldebaran2022 <110170715+aldebaran2022@users.noreply.github.com>
2022-08-08 15:02:58 +0200

    Update benutzer_db_istj.sql

--- benutzer_db_istj.sql
+++ benutzer_db_istj.sql
@@ -1,4 +1,4 @@
 insert into benutzer (vorname,nachname,benutzername,email,bild,geschlecht) values
-('Adam','Barr','adambarr','adamb@contoso.com','../uploads/files/adam.jpg','m'),
-('Alan','Steiner','alansteiner','alans@contoso.com','../uploads/files/alan.jpg','d'),
-('Christine','Koch','christine','christk@contoso.com','../uploads/files/christine.jpg','w');
+('Adam','Barr','adambarr','adamb@contoso.com','../../uploads/files/adam.jpg','m'),
+('Alan','Steiner','alansteiner','alans@contoso.com','../../uploads/files/alan.jpg','d'),
+('Christine','Koch','christine','christk@contoso.com','../../uploads/files/christine.jpg','w');
