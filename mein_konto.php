3616c6a9fcdacfff28fbc63490569d716ef57e4a
tschuppich <110220276+tschuppich@users.noreply.github.com>
2022-08-08 09:50:09 +0200

    am 080822 bearbeitet

--- mein_konto.php
+++ mein_konto.php
@@ -2,13 +2,17 @@
 // Include config file
 require_once "../config.php";
 
-$benutzername = $id = $benutzernameERR = $idErr = "";
+$benutzername = $id = $id_score = $benutzernameERR = $idErr = $scoreErr = "";
 $zaehler = 0;
 $spaltennamen = array("ID","Vorname","Nachname","Benutzername","Email","Anmeldezeitpunkt","Bild","Geschlecht");
 //select mit prepared statement:
 $sql = "select * from benutzer where id = ? and benutzername = ?;";
 $stmt = mysqli_prepare($link, $sql);
 mysqli_stmt_bind_param($stmt, 'is', $id, $benutzername);
+
+$sql_score = "select spruch from score where id = ?";
+$stmt_score = mysqli_prepare($link, $sql_score);
+mysqli_stmt_bind_param($stmt_score, 'i', $id);
 
 function test_input($data) {
 	  $data = trim($data);	//entferne space, tab und newline
@@ -62,11 +66,11 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 			$benutzername = test_input($_POST["benutzername"]);					
 			if (!preg_match("/^[ÄÜÖäüößa-zA-Z-]*[0-9]*$/",$benutzername)) {
 			  $benutzernameERR = "Es sind nur Buchstaben und Zahlen erlaubt";
-			}else{
-				$zaehler +=1;
-				//speichere, dass Eingabe ok
-			}			
-	  }
+			}	else{
+					$zaehler +=1;
+					//speichere, dass Eingabe ok
+				}			
+		}
 	if (empty($_POST["id"])) {
 		$idErr = "Eingabe einer ID ist erforderlich!";
 	  } else {  
@@ -77,7 +81,18 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 				$zaehler +=1;
 				//speichere, dass Eingabe ok
 			}
-	  }
+		}
+	if (empty($_POST["id_score"])) {
+		$scoreErr = "Eingabe eines Scores (0-5) ist erforderlich!";
+	  } else {
+			$id_score = test_input($_POST["id_score"]);					
+			if (!preg_match("/^[0-9]*$/",$id_score)) {
+			  $scoreErr = "Es sind nur Zahlen erlaubt";
+			}else{
+				$zaehler +=1;
+				//speichere, dass Eingabe ok
+			}			
+		}
 }
 
 ?>
@@ -155,12 +170,46 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 <div style="color:white;font-size:1.1em;">	
 	<form method=POST action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
 		<label for="benutzername" style="color:white;">Bitte geben Sie Ihren Benutzernamen ein:
-		<b class="error"  style="color:red;">* <?php echo "<br>".$benutzernameERR;?></b></label>
-		<input name="benutzername" type="text" class="feedback-input" placeholder="Benutzername"  value="<?php echo $benutzername;?>">
+		<b class="error"  style="color:red;">* 
+		<?php 
+			echo "<br>".$benutzernameERR;
+		?>
+		
+		</b></label>
+		<input name="benutzername" type="text" class="feedback-input" placeholder="Benutzername"  value="
+		<?php
+			echo $benutzername;
+		?>
+		">
 
 
 		<label for="id" style="color:white;">Bitte geben Sie Ihre ID ein: <b class="error"  style="color:red;">* <?php echo "<br>".$idErr;?></b></label>		
 		<input name="id" type="number" class="feedback-input" placeholder="ID"  min="1" max="1000" value="<?php echo $id;?>">
+
+		  <p>
+        <label for="id_score">Bitte wählen Sie: <b class="error" style="color:red;">* 
+		
+		<?php 
+			echo $scoreErr;
+		?>
+		</b></label>
+        <select name="id_score" id="id_score" >
+            <option value="
+					<?php 
+						echo $id_score;
+					?>"><?php echo $id_score;?></option>
+            
+            <option value="0">0</option>
+			<option value="1">1</option>
+			<option value="2">2</option>
+			<option value="3">3</option>
+			<option value="4">4</option>
+			<option value="5">5</option>
+			      
+        </select>
+    
+<br>
+    </p>
 
 		<input type="submit" value="Absenden">
 		<br>
