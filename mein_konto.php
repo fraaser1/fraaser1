af0ab5b10ef1e4be1b47d56bd03b6e5acdc43e4d
tschuppich <110220276+tschuppich@users.noreply.github.com>
2022-08-08 11:06:11 +0200

    Sollte so ok sein

--- mein_konto.php
+++ mein_konto.php
@@ -2,24 +2,29 @@
 // Include config file
 require_once "../config.php";
 
-$benutzername = $id = $id_score = $benutzernameERR = $idErr = $scoreErr = "";
-$zaehler = 0;
+$benutzername = $id = $benutzernameERR = $idErr =  $id_score = $scoreErr = "";
+$zaehler = $id_score_int = 0;
 $spaltennamen = array("ID","Vorname","Nachname","Benutzername","Email","Anmeldezeitpunkt","Bild","Geschlecht");
 //select mit prepared statement:
+//Prepare-Statement für Tabelle benutzer
 $sql = "select * from benutzer where id = ? and benutzername = ?;";
 $stmt = mysqli_prepare($link, $sql);
 mysqli_stmt_bind_param($stmt, 'is', $id, $benutzername);
 
-$sql_score = "select spruch from score where id = ?";
+//Prepare-Statement für Tabelle score
+$sql_score = "select spruch from score where id = ?;";
 $stmt_score = mysqli_prepare($link, $sql_score);
-mysqli_stmt_bind_param($stmt_score, 'i', $id);
+mysqli_stmt_bind_param($stmt_score, 'i', $id_score_int);
 
+/*function test_input($data) {
+	//entfernt alle ASCII außer Buchstaben,also auch Zahlen
+	$data = trim($data,"\x00..\x2F\x3A..\x40\x7B..\x7F\x5B..\x60");
+	return $data;
+	*/
 function test_input($data) {
 	  $data = trim($data);	//entferne space, tab und newline
-	  $data = stripslashes($data);	//entferne backslash (macht nur den Backslash vor einem Backslash weg weg
-	  $data = htmlspecialchars($data); //maskiere < und >
-	  //entfernt alle ASCII außer Buchstaben,also auch Zahlen
-	  //$data = trim($data,"\x00..\x2F\x3A..\x40\x7B..\x7F\x5B..\x60");
+	  $data = stripslashes($data);	//entferne backslash (macht nur den Backslash vor einem Backslash weg weg)
+	  $data = htmlspecialchars($data); //maskiere < und >		
 	  return $data;
 }
 
@@ -60,39 +65,43 @@ function erstelle_Tabelle($spaltennamen,$ausgabe){
 }
 //hier wäre alternativ auch isset möglich:
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-	 if (empty($_POST["benutzername"])) {
+	if (empty($_POST["benutzername"])) {
 		$benutzernameERR = "Eingabe eines Benutzernamens ist erforderlich!";
 	  } else {
 			$benutzername = test_input($_POST["benutzername"]);					
-			if (!preg_match("/^[ÄÜÖäüößa-zA-Z-]*[0-9]*$/",$benutzername)) {
-			  $benutzernameERR = "Es sind nur Buchstaben und Zahlen erlaubt";
-			}	else{
+				if (!preg_match("/^[ÄÜÖäüößa-zA-Z-]*[0-9]*$/",$benutzername)) {
+				  $benutzernameERR = "Es sind nur Buchstaben und Zahlen erlaubt";
+				}else{
 					$zaehler +=1;
 					//speichere, dass Eingabe ok
-				}			
+				}
+			
 		}
 	if (empty($_POST["id"])) {
 		$idErr = "Eingabe einer ID ist erforderlich!";
 	  } else {  
-		$id = test_input($_POST["id"]);							
-			if (!preg_match("/^[0-9]*$/",$id)) {
-				  $idErr = "Es sind nur Zahlen erlaubt";
-			}else{
-				$zaehler +=1;
-				//speichere, dass Eingabe ok
-			}
-		}
+			$id = test_input($_POST["id"]);							
+				if (!preg_match("/^[0-9]*$/",$id)) {
+					  $idErr = "Es sind nur Zahlen erlaubt";
+				}else{
+					$zaehler +=1;
+					//speichere, dass Eingabe ok
+				}
+	  }
 	if (empty($_POST["id_score"])) {
 		$scoreErr = "Eingabe eines Scores (0-5) ist erforderlich!";
 	  } else {
-			$id_score = test_input($_POST["id_score"]);					
+			$id_score = $_POST['id_score'];
 			if (!preg_match("/^[0-9]*$/",$id_score)) {
 			  $scoreErr = "Es sind nur Zahlen erlaubt";
 			}else{
 				$zaehler +=1;
 				//speichere, dass Eingabe ok
+				$id_score_int = intval($id_score);
+				echo $id_score_int;
 			}			
 		}
+
 }
 
 ?>
@@ -100,11 +109,11 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 <!DOCTYPE html>
 <html lang="de">
 <head> 
-	 <meta charset="utf-8">
-	 <meta id="viewport" name="viewport" content="width=device-width, initial-scale=1.0">
-		<link rel="icon" type="image/icon" sizes="16x16" href="favicon.ico">
-		<link rel="stylesheet" href="quiz.css"> 
-		<link rel="stylesheet" href="kontakte.css">
+		 <meta charset="utf-8">
+		 <meta id="viewport" name="viewport" content="width=device-width, initial-scale=1.0">
+			<link rel="icon" type="image/icon" sizes="16x16" href="favicon.ico">
+			<link rel="stylesheet" href="quiz.css"> 
+			<link rel="stylesheet" href="kontakte.css">
 			
 <style>
 	table {
@@ -167,71 +176,66 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 		<h2 class="text-dark"> Teste dein Wissen?</h2>	
 	</div>
 </header>
-<div style="color:white;font-size:1.1em;">	
-	<form method=POST action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
-		<label for="benutzername" style="color:white;">Bitte geben Sie Ihren Benutzernamen ein:
-		<b class="error"  style="color:red;">* 
-		<?php 
-			echo "<br>".$benutzernameERR;
-		?>
-		
-		</b></label>
-		<input name="benutzername" type="text" class="feedback-input" placeholder="Benutzername"  value="
-		<?php
-			echo $benutzername;
-		?>
-		">
-
-
-		<label for="id" style="color:white;">Bitte geben Sie Ihre ID ein: <b class="error"  style="color:red;">* <?php echo "<br>".$idErr;?></b></label>		
-		<input name="id" type="number" class="feedback-input" placeholder="ID"  min="1" max="1000" value="<?php echo $id;?>">
-
-		  <p>
-        <label for="id_score">Bitte wählen Sie: <b class="error" style="color:red;">* 
-		
-		<?php 
-			echo $scoreErr;
-		?>
-		</b></label>
-        <select name="id_score" id="id_score" >
-            <option value="
-					<?php 
-						echo $id_score;
-					?>"><?php echo $id_score;?></option>
-            
-            <option value="0">0</option>
-			<option value="1">1</option>
-			<option value="2">2</option>
-			<option value="3">3</option>
-			<option value="4">4</option>
-			<option value="5">5</option>
+<div style="color:white;font-size:1.1em;">
+	
+		<form method=POST action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+			<label for="benutzername" style="color:white;">Bitte geben Sie Ihren Benutzernamen ein:
+			<b class="error"  style="color:red;">* <?php echo "<br>".$benutzernameERR;?></b></label>
+			<input name="benutzername" type="text" class="feedback-input" placeholder="Benutzername"  value="<?php echo $benutzername;?>">
+			
+			
+			<label for="id" style="color:white;">Bitte geben Sie Ihre ID ein: 
+			<b class="error"  style="color:red;">* <?php echo "<br>".$idErr;?></b></label>		
+			<input name="id" type="number" class="feedback-input" placeholder="ID"  min="1" max="1000" value="<?php echo $id;?>">
+			
+			
+			<label for="id_score">Bitte wählen Sie: <b class="error" style="color:red;">*<?php echo $scoreErr;?></b></label>		
+			<select name="id_score" id="id_score" >
+				<option value="<?php echo $id_score;?>"><?php echo $id_score;?></option>
+				
+				<option value="0">0</option>
+				<option value="1">1</option>
+				<option value="2">2</option>
+				<option value="3">3</option>
+				<option value="4">4</option>
+				<option value="5">5</option>
 			      
-        </select>
-    
-<br>
-    </p>
-
-		<input type="submit" value="Absenden">
-		<br>
-		<b class="error"  style="color:red;">* Pflichtfeld</b>
-	</form>
-<div style="text-align:center;color:white;font-size:1.1em;">	
+			</select>
+			<br>				
+			<p>
+			<input type="submit" value="Absenden">
+			</p>
+			<b class="error"  style="color:red;">* Pflichtfeld</b>
+			
+		</form>
+	<div style="text-align:center;color:white;font-size:1.1em;">	
 		<?php
-			if ($zaehler == 2){
+			if ($zaehler == 3){
 				if(mysqli_stmt_execute($stmt)){
 					echo "<br>Die Datenabfrage hat geklappt.<br>";
 					if ($ausgabe = mysqli_fetch_assoc(mysqli_stmt_get_result($stmt))){
-						mysqli_stmt_close($stmt);
+						mysqli_stmt_close($stmt);						
 						echo "<br>";
 						begruessung($ausgabe);
-						echo "<br>";
+						
 						erstelle_Tabelle($spaltennamen,$ausgabe);
 						echo "<br>";
+						
 					}else{
 						echo "<br>Zu diesem Benutzernamen und dieser ID sind keine Daten vorhanden<br><br>"; 
 					}
 				}else{
-					echo "<br>Die Datenabfrage hat nicht geklappt.<br><br>";
+					echo "<br>Die Datenabfrage hat nicht geklappt.<br>";
+				}
+			    if(mysqli_stmt_execute($stmt_score)){
+					echo "Ihr Score ist ". $id_score_int . "<br>";
+					if ($spruch_ausgabe = mysqli_fetch_assoc(mysqli_stmt_get_result($stmt_score))){
+						mysqli_stmt_close($stmt_score);
+						echo $spruch_ausgabe['spruch'];
+						echo "<br><br>";
+					}else {echo "Kein Spruch zu Ihrem Score vorhanden";};
+				}else{
+					echo "<br>Die Datenabfrage score hat nicht geklappt.<br>";
 				}
 			}
 
