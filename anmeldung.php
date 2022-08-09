2fd6af39fe182c3346754a66475e0640f01cbee6
fraaser1 <97669552+fraaser1@users.noreply.github.com>
2022-08-09 13:18:11 +0200

    Add files via upload

--- anmeldung.php
+++ anmeldung.php
@@ -1,14 +1,7 @@
 <?php
-require_once "../php/config.php";
+require_once "config.php";   
  
-		
-         
- <?php
-require_once "../php/config.php";
- 
-		
-         
- $geschlecht = $vorname = $nachname = $begruessung = $benutzername = $email = $localFileName =  $id =$emailErr = $benutzernameErr = $vornameErr = $nachnameErr = $geschlechtErr = $anrede = "";
+$geschlecht = $vorname = $nachname = $begruessung = $benutzername = $email = $localFileName =  $id =$emailErr = $benutzernameErr = $fileErr = $vornameErr = $nachnameErr = $geschlechtErr = $anrede = "";
 $zaehler = 0;
 
 $allowed_files = [
@@ -196,7 +189,7 @@ if (empty($_POST["nachname"])) {
 </header>
 <div class="body">
 	
-	<form method = "POST"  action ="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>#ausgabe"  enctype="multipart/form-data">
+	<form method = "POST"  action ="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>"  enctype="multipart/form-data">
 		<p> Um sich Registrieren zu können füllen Sie bitte die Felder vollständig aus:<br><br></p>
 		 		 
 			<label for="geschlecht"  style="color:white;">Bitte wählen Sie: <b class="error" style="color:red;"> *<?php echo $geschlechtErr;?></b> </label>		
@@ -221,9 +214,10 @@ if (empty($_POST["nachname"])) {
 			<label for="email"  style="color:white;">Bitte geben Sie Ihre Email-Adresse ein:<b class="error" style="color:red;"> *<?php echo $emailErr;?></b> </label>
 			<input type="text" name="email" id="email"  class="feedback-input"placeholder="Email-Adresse" value="<?php echo $email;?>">
     			
-			<input type="hidden" name="MAX_FILE_SIZE" value="2000000">
+			<!--<input type="hidden" name="MAX_FILE_SIZE" value="2000000">-->
 			<label  style="color:white;">Bitte wählen Sie ein Bild (*.jpg, *.png, *.gif oder *.pdf) zum Hochladen aus.</label>
-			<input name="bild" type="file" accept="image/gif,image/jpeg,image/png,application/pdf" value="<?php echo $localFileName;?>"> 
+			<input name="bild" type="file" accept="image/gif,image/jpeg,image/png,application/pdf"  value="<?php echo $localFileName;?>"> 
+			<b class="error"  style="color:red;"><?php echo $fileErr;?></b>
 			
 			<button type="submit" name = "speichern" id="speichern">Anmelden</button>
 		</p>
@@ -231,14 +225,14 @@ if (empty($_POST["nachname"])) {
 	
 	</form>
 </div>		
-<div id="ausgabe" style="text-align:center;color:white;font-size:1.1em;">
+<div style="text-align:center;color:white;font-size:1.1em;">
 	<?php
 		if($zaehler >= 5){
-			if(mysqli_stmt_execute($stmt)){ 
+			if(mysqli_stmt_execute($stmt)){ 					
 					echo "Speichern hat geklappt.";
 					echo "<br>";
 					$last_id = mysqli_insert_id($link);
-					echo "Hallo " . $anrede . " " . $nachname . ",<br>vielen Dank für die Registrierung!<br>Bitte notieren sie die folgenden Infos für den Abruf Ihrer Daten:<br> Ihr Benutzername ist " . $benutzername . "<br>und Ihre ID ist " . $last_id .".<br>Viel Spaß";
+					echo "Hallo " . $anrede . " " . $nachname . ",<br>Vielen Dank für die Registrierung!<br>Bitte notieren Sie die folgenden Infos für den Abruf Ihrer Daten:<br> Ihr Benutzername ist " . $benutzername . "<br>und ihre ID ist " . $last_id .".<br>Viel Spaß";
 					echo "<br>";
 					
 			
@@ -252,11 +246,9 @@ if (empty($_POST["nachname"])) {
 </div>
 
 
+
 <div class="robbi"></div>
 			
 </body>
 	   
 </html>
-	   
-	   </html>
-
